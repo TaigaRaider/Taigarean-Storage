02d595009a3a780db18aa353293c355755ffcfc5
Taigaraider <taigatimothy5@gmail.com>
2026-01-18 13:43:40 +0100

    I refactored, improving readability and easing maintenance.

--- python projects/sentence iterator.py
+++ python projects/sentence iterator.py
@@ -1,13 +1,17 @@
 import time
 
-import os
+# sentence = input(f'Enter a sentence ')
+# sentences = [x for x in sentence]
+#
+# print(sentences, end='')
+# time.sleep(0.1)
 
-sentence = input(f'Enter a sentence ')
-sentences = [x for x in sentence]
 
+sentence = input(f'Enter a sentence: ')
+sentences = []
 
-
-
-print(sentences, end='')
-time.sleep(0.1)
-
+for x in sentence:
+    sentences.append(x)
+for item in sentences:
+    print(sentences, end='')
+    time.sleep(0.1)
