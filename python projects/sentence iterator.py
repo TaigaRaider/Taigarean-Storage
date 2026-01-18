151f6d513bd591093c1aba840da50353ea4b728d
Taigaraider <taigatimothy5@gmail.com>
2026-01-18 14:09:44 +0100

    I refactored, improving readability and easing maintenance.

--- python projects/sentence iterator.py
+++ python projects/sentence iterator.py
@@ -1,17 +1,21 @@
 import time
-
+import os
 # sentence = input(f'Enter a sentence ')
 # sentences = [x for x in sentence]
 #
 # print(sentences, end='')
-# time.sleep(0.1)
 
 
 sentence = input(f'Enter a sentence: ')
 sentences = []
 
-for x in sentence:
-    sentences.append(x)
-for item in sentences:
-    print(sentences, end='')
-    time.sleep(0.1)
+def read_write_line():
+    for x in sentence:
+        sentences.append(x)
+    for item in sentences:
+        print(item, end='')
+        time.sleep(0.1)
+    time.sleep(2)
+    os.system("cls")       #clears terminal after printing; still looking for a better to wiping the entire terminal
+
+read_write_line()
