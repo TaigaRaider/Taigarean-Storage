2421121af8cbe3f83b75046342d080bea8dc214d
Taigaraider <taigatimothy5@gmail.com>
2026-02-26 22:12:20 +0100

    I created this new max function to find the largest integer in an array!

--- python projects/max function.py
+++ python projects/max function.py
@@ -6,41 +6,65 @@ arr = []
 index = 0
 
 
-def get_work_members():
-    while index < members:
+def int_error():
+    print(f"Enter ONLY INTEGERS")
+
+
+def get_work_members(members, work_index):
+    work_array = []
+    while work_index < members:
         try:
-            member = int(input(f"Enter a number"))
-            arr.append(member)
+            member = int(input(f"Enter a number "))
+            work_array.append(member)
+            work_index += 1
+            print(f"{work_array}, {work_index}")
         except ValueError:
-            get_work_members()
+            int_error()
+            get_work_members(members, work_index)
+
+        if work_index == members:
+            print("Done!")
+    return work_array
 
 
-def work_main():
-    members = int(input(f"How many numbers are in the group? "))
+def define_number_of_members():
+    try:
+        members = int(input(f"How many numbers are in the group? "))
 
-    for member in arr:
-        maximum = arr[0]
-        if member > maximum:
-            maximum = member
-    return maximum
+        return members
+    except ValueError:
+        int_error()
+        define_number_of_members()
 
 
-def work_fun():
-    decision = input(f"{Fore.BLUE}Do you want to have fun or do you want to just work? ")
+def work():
+    work_array = get_work_members(define_number_of_members(), 0)
+    work_maximum = work_array[0]
+
+    for member in work_array:
+        if member > work_maximum:
+            work_maximum = member
+    return work_maximum
+
+
+def main():
+    decision = input(
+        f"{Fore.RED}Do you want to have {Fore.YELLOW}fun{Fore.RED} or do you want to just {Fore.CYAN}work? ").strip().lower()
     if decision == "work":
-        workmax = work_main()
-        return workmax
+        return print(work())
     elif decision == "fun":
-        fun_main()
+        return fun()
     else:
-        pass
+        print("ERROR Invalid input!")
+        return main()
+
 
 def get_numberofmembers():
     try:
         numberofmembers = int(input("How many numbers should your array contain? "))
         return numberofmembers
     except ValueError:
-        print(f"Enter ONLY INTEGERS")
+        int_error()
         get_numberofmembers()
 
 
@@ -49,26 +73,25 @@ def define_lower_boundary():
         lower_boundary = int(input("What should be the lower boundary of the array? "))
         return lower_boundary
     except ValueError:
-        print(f"Enter ONLY INTEGERS")
+        int_error()
         define_lower_boundary()
 
 
-def define_upper_boundary():
+def define_upper_boundary(lower_boundary):
     try:
         upper_boundary = int(input("What should be the upper boundary of the array? "))
         upper_boundary = upper_boundary - random.randint(0, 10)
         return upper_boundary
     except ValueError:
-        print(f"Enter ONLY INTEGERS")
+        int_error()
         define_upper_boundary()
-
-
 
 
 def generate_numbers(numberofmembers, lower_boundary, upper_boundary):
     for member in range(numberofmembers):
         member = random.randint(lower_boundary, upper_boundary)
         arr.append(member)
+
 
 def find_largest_member():
     while index <= len(arr) - 1:
@@ -81,17 +104,22 @@ def find_largest_member():
     print(arr)
 
 
-def fun_main():
-    lowerBoundary = define_lower_boundary()
-    upperBoundary = define_upper_boundary()
-    numberOfMembers = get_numberofmembers()
+def fun():
+    lowerboundary = define_lower_boundary()
+    upperboundary = define_upper_boundary()
+    numberofmembers = get_numberofmembers()
 
-    generate_numbers(numberOfMembers, lowerBoundary, upperBoundary)
+    generate_numbers(numberofmembers, lowerboundary, upperboundary)
     print(arr)
-    print(f"The Lower Boundary of this selection is: {lowerBoundary}")
-    print(f"The Upper Boundary of this selection is: {upperBoundary}")
-    print(f"The Number of Members in this selection is: {numberOfMembers}")
+    print(f"The Lower Boundary of this selection is: {lowerboundary}")
+    print(f"The Upper Boundary of this selection is: {upperboundary}")
+    print(f"The Number of Members in this selection is: {numberofmembers}")
     find_largest_member()
 
 
-work_fun()
+if __name__ == '__main__':
+    main()
+
+#         if lower_boundary < upper_boundary:
+#             upper_boundary = upper_boundary
+#         else:
