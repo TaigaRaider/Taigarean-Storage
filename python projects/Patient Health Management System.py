b3f5d760f341b852d5c76aac50d2b309c5c7b955
Taigaraider <taigatimothy5@gmail.com>
2026-03-03 14:20:20 +0100

    I started commenting

--- python projects/Patient Health Management System.py
+++ python projects/Patient Health Management System.py
@@ -1,4 +1,8 @@
 class  User:
+    def __init__(self, username, account_state):
+        self.username= username
+        self.account_state= account_state
+
     def login(self):
         pass
     def logout(self):
