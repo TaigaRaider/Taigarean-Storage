fe7f2e03156f52710ee2b37dc8969c56cdd780e8
Taigaraider <taigatimothy5@gmail.com>
2026-04-21 20:39:36 +0100

    lets get started

--- python projects/COMMERCE and SEARCH AGENTS/main.py
+++ python projects/COMMERCE and SEARCH AGENTS/main.py
@@ -1,20 +1,25 @@
 from rich.prompt import Prompt
-
+import json
 INVENTORY__FILE = "inventory.txt"
+PRODUCT_FILE = "product_specification.json"
 
-Product = Prompt.ask('What do you seek? ')
-
+# product_prompt = Prompt.ask('What do you seek? ').upper().strip().rsplit(sep= " ")
+# product_name = product_prompt[1]
 
 def productCheck(item, inventory_dict):
     is_present: bool = False
-
     if item in inventory_dict:
         is_present = True
-
         return is_present
     else:
-        return f"Error 404: Item could not be found"
+        return f"Error 404: could not be found"
 
+def furtherProductSpecification():
+    pass
+    # test1
+    # test2
+    # test3
+    # test4
 
 def fetchInventory():
     with open(INVENTORY__FILE, "r") as file:
@@ -25,4 +30,7 @@ def fetchInventory():
     return available_items
 
 
-print(inventoryCheck(Product, displayInventory()))
+with open(PRODUCT_FILE, "r") as file:
+    content = json.load(file)
+    for line in content:
+        print(line)
